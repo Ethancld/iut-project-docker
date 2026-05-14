31c49f1d70a1ca8d9a0c2799cfb68bfc61133298
user <user@git.myguest.virtualbox.org>
2026-05-14 15:27:54 +0200

    Exercice 1 : Fusion et résolution de conflit terminées

--- src/main/java/org/example/iutprojectdocker/HelloController.java
+++ src/main/java/org/example/iutprojectdocker/HelloController.java
@@ -1,16 +1,40 @@
 package org.example.iutprojectdocker;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
 @RestController
 public class HelloController {
 
+    private Long getHitCountSafe() throws InterruptedException {
+        int retries = 5;
+
+        while (retries >= 0) {
+            try (Jedis jedis = new Jedis("redis", 6379)) {
+                return jedis.incr("hits");
+            } catch (Exception e) {
+                if (retries == 0) {
+                    return null;
+                }
+                retries--;
+                Thread.sleep(500);
+            }
+        }
+        return null;
+    }
 
     @GetMapping("/")
     public String hello() {
         try {
-            return "Hello World";
+            String appUser = System.getenv("APP_USER");
+            String name = (appUser == null || appUser.isEmpty()) ? "World" : appUser;
+            Long count = getHitCountSafe();
+            String base = String.format("Hello %s!", name);
+            
+            return (count == null)
+                    ? base + " (Redis indisponible)\n"
+                    : base + String.format(" J'ai été visité %d fois.\n", count);
+
         } catch (Exception e) {
             return "Error: " + e.getMessage();
         }
